aa1878f606216dfd2cb5987ed496945115879dce
Will Elson <elson594@gmail.com>
2016-05-27 10:23:55 +0100

    Added routes and route info to the database

--- app/models.py
+++ app/models.py
@@ -5,9 +5,11 @@ from datetime import datetime
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column('username', db.String(20), unique=True , index=True)
-    password = db.Column('password' , db.String(10))
+    password = db.Column('password' , db.String(30))
     email = db.Column('email',db.String(50),unique=True , index=True)
     registered_on = db.Column('registered_on' , db.DateTime)
+
+    routes = db.relationship('Route', cascade="all, delete-orphan", backref='user', lazy='dynamic')
 
     def __init__(self , username ,password , email):
         self.username = username
@@ -39,3 +41,28 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.id)
+
+
+
+class Route(db.Model):
+    """One to many relationship with user"""
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    route_info = db.relationship('RouteInfo', cascade="all, delete-orphan", backref='route', lazy='dynamic')
+    name = db.Column('name' , db.String(10))
+
+class RouteInfo(db.Model):
+    """One to many relationship with route"""
+    id = db.Column(db.Integer, primary_key=True)
+    route_id = db.Column(db.Integer, db.ForeignKey('route.id'))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    elevation = db.Column(db.Float)
+
+
+
+
+
+
+
+#
